c80a9528ddd9d2b307db20e79dd3326f75728ec2
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-30 22:32:42 +0530

    Fix formatting in UserDetailsServiceImpl.java

--- src/main/java/com/klu/carrental/security/UserDetailsServiceImpl.java
+++ src/main/java/com/klu/carrental/security/UserDetailsServiceImpl.java
@@ -24,5 +24,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                         .roles("USER")  // Modify roles as needed
                         .build())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
     }
 }
